ae32f594597dfc330c30954dc9966e1e05b316c1
Brien Smarandache <briensmarandache@gmail.com>
2022-04-01 10:36:53 -0400

    initial commit

---
+++ app/models/comment.rb
@@ -0,0 +1,8 @@
+class Comment < ApplicationRecord
+  include Visible
+
+  belongs_to :article
+
+  validates :commenter, presence: true, length: {minimum: 3, maximum: 20}
+  validates :body, presence: true, length: {minimum: 3, maximum: 250}
+end
---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+
+  get 'about', to: "pages#about"
+  # get "/articles", to: "articles#index"
+  # The route above declares that "GET /articles" requests are mapped to the "index" action of "ArticlesController"
+  # get "/articles/:id", to: "articles#show"
+  # The route above declares that "GET /articles/:id" requests are mapped to the "show" action of "ArticlesController";
+  #   where ":id" is a route parameter accessible by the "show" action of "ArticlesController" using params[:id] 
+  
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root "pages#home"
+
+  resources :articles do
+    resources :comments
+  end
+end
